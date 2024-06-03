ead5b1802a3eb5790829cb3e6a23d8acef4069af
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-02 23:01:00 -0500

    Utilizando un for dentro de index.html para agregar los divs con los 3 destinos, ademas que se crearon mas objetos los cuales son los que se usaron

--- proyects/telusko/telusko/travello/views.py
+++ proyects/telusko/telusko/travello/views.py
@@ -7,7 +7,21 @@ def index(request):
     dest1 = Destination()
     dest1.name = 'Mumbai'
     dest1.desc = 'That City That Never Sleeps'
+    dest1.img = 'destination_1.png'
     dest1.price = 700
 
+    dest2 = Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc = 'First Biryani, Then Sherwani'
+    dest2.img = 'destination_2.png'
+    dest2.price = 650
 
-    return render(request, 'index.html', {'dest1' : dest1})
+    dest3 = Destination()
+    dest3.name = 'Bengaluru'
+    dest3.desc = 'Beautiful City'
+    dest3.img = 'destination_3.png'
+    dest3.price = 750
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests' : dests})
